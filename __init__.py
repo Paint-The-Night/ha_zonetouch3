d5c90e5e356652eff48868b0971719e11d2e44cc
Paint The Night <3980690+Paint-The-Night@users.noreply.github.com>
2025-01-03 15:15:39 +1000

    Add async setup function to initialize the integration

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
 """The zone touch 3 integration."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Zone Touch 3 integration."""
+    # Perform any necessary initialization here
+    return True
